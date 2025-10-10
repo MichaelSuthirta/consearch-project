60798a725b22f0a1fcfe156c7b7e9d7364744995
MichaelSuthirta <michaelsuthirta@gmail.com>
2025-10-10 22:12:30 +0700

    Feat: Created scroll navigation
    Added tap navigation in case scroll failed

--- consearch/lib/UserAuthPages/RegisterPage.dart
+++ consearch/lib/UserAuthPages/RegisterPage.dart
@@ -1,14 +1,53 @@
+import 'package:consearch/main.dart';
 import 'package:flutter/material.dart';
 import '/UI/DisplayConstants.dart';
 import '/UI/GradientBackground.dart';
 
-class RegisterPage extends StatelessWidget{
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
   @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage>{
+
+  void navigate(DragEndDetails details){
+    if(details.primaryVelocity!.compareTo(0) == 1){
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MyHomePage(title: "Consearch")
+          )
+      );
+    }
+  }
+
+  @override
   Widget build(BuildContext context){
-    return GradientBackground(
-        content: Text("Test")
+    return GestureDetector(
+      onVerticalDragEnd: (DragEndDetails details) => navigate(details),
+      onTap: (){
+        Navigator.push(
+            context, MaterialPageRoute(
+              builder: (context) => MyHomePage(title: "Consearch")
+            )
+        );
+      },
+      child: GradientBackground(
+        content: Container(
+          decoration: BoxDecoration(
+            color: DisplayConstants.getGreyFrameColor()
+          ),
+          height: 200,
+          width: 200,
+          child: Column(
+            children: [
+              Text("A")
+            ]
+          ),
+        ),
+      )
     );
   }
 }
--- consearch/lib/main.dart
+++ consearch/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:consearch/UserAuthPages/RegisterPage.dart';
 import 'package:flutter/material.dart';
-import '/UI/DisplayConstants.dart';
 import '/UI/GradientBackground.dart';
 
 void main() {
@@ -35,74 +35,94 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  void navigate(DragEndDetails details){
+    if(details.primaryVelocity!.compareTo(0) == -1){
+      Navigator.push(
+          context, MaterialPageRoute(
+            builder: (context) => RegisterPage()
+          )
+      );
+    }
+    else{
+      return;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GradientBackground(
-      // appBar: AppBar(
-      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      //   title: Text(widget.title),
-      // ),
-      content: Flex(
-          direction: Axis.vertical,
-          children: [
-            const Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "CONSEARCH",
-                    style: TextStyle(
-                      fontFamily: "Lexend Mega",
-                      fontWeight: FontWeight.w700,
-                      fontSize: 40
-                    ),
-                  ),
-                  Text(
-                    "YOUR DAILY DOSE OF CONCERT",
-                    style: TextStyle(
-                        fontFamily: "Lexend Mega",
-                        fontWeight: FontWeight.w700,
-                        fontSize: 10
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
-              alignment: Alignment.bottomCenter,
-              child: const Column(
-                children: [
-                  Text(
-                    "SCROLL TO GET STARTED",
-                    style: TextStyle(
-                      fontFamily: "Lexend",
-                      fontWeight: FontWeight.w700,
-                      fontSize: 10
-                    ),
-                  ),
-                  Image(
-                    image: AssetImage("assets/UI/Icons/Arrow Down.png"),
-                    width: 16.34,
-                    height: 20,
-                    opacity: AlwaysStoppedAnimation(0.45),
-                  ),
-                  Image(
-                    image: AssetImage("assets/UI/Icons/Arrow Down.png"),
-                    width: 21.25,
-                    height: 26,
-                    opacity: AlwaysStoppedAnimation(0.75),
-                  ),
-                  Image(
-                    image: AssetImage("assets/UI/Icons/Arrow Down.png"),
-                    width: 27.91,
-                    height: 34.16
-                  )
-                ],
-              )
+    return GestureDetector(
+      //To detect scroll. When scroll is detected, navigate to next page.
+      onVerticalDragEnd: (DragEndDetails details) => navigate(details),
+      onTap: (){
+        Navigator.push(
+            context, MaterialPageRoute(
+              builder: (context) => RegisterPage()
             )
-          ],
-        )
+        );
+      },
+      child: GradientBackground(
+          content: Flex(
+            direction: Axis.vertical,
+            children: [
+              const Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "CONSEARCH",
+                      style: TextStyle(
+                          fontFamily: "Lexend Mega",
+                          fontWeight: FontWeight.w700,
+                          fontSize: 40
+                      ),
+                    ),
+                    Text(
+                      "YOUR DAILY DOSE OF CONCERT",
+                      style: TextStyle(
+                          fontFamily: "Lexend Mega",
+                          fontWeight: FontWeight.w700,
+                          fontSize: 10
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                  alignment: Alignment.bottomCenter,
+                  child: const Column(
+                    children: [
+                      Text(
+                        "SCROLL TO GET STARTED",
+                        style: TextStyle(
+                            fontFamily: "Lexend",
+                            fontWeight: FontWeight.w700,
+                            fontSize: 10
+                        ),
+                      ),
+                      Image(
+                        image: AssetImage("assets/UI/Icons/Arrow Down.png"),
+                        width: 16.34,
+                        height: 20,
+                        opacity: AlwaysStoppedAnimation(0.45),
+                      ),
+                      Image(
+                        image: AssetImage("assets/UI/Icons/Arrow Down.png"),
+                        width: 21.25,
+                        height: 26,
+                        opacity: AlwaysStoppedAnimation(0.75),
+                      ),
+                      Image(
+                          image: AssetImage("assets/UI/Icons/Arrow Down.png"),
+                          width: 27.91,
+                          height: 34.16
+                      )
+                    ],
+                  )
+              )
+            ],
+          )
+      )
     );
   }
 }
